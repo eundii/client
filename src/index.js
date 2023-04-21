3c885677a8220bc6f00aabdb1870b9a02db69f81
eundii <eunjee1504@naver.com>
2023-04-21 12:23:51 +0900

    에러 수정

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./Reducer/store";
+
 import "../src/scss/reset.scss";
 import App from "./App";
-
-import store from "./Reducer/store";
-import { Provider } from "react-redux";
 
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
